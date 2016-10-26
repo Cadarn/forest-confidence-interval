4c4f09309c6d2d6604d87d8a7123034598b2923b
Ondrej Urban <ondrej@stanford.edu>
2016-10-26 16:38:01 +0100

    Added calibration agains MC noise effects.

--- forestci/forestci.py
+++ forestci/forestci.py
@@ -1,9 +1,12 @@
+import math
 import numpy as np
+from scipy.stats import mstats, norm
+from scipy.optimize import minimize
 from sklearn.ensemble.forest import _generate_sample_indices
 from .due import _due, _BibTeX
 
 __all__ = ["calc_inbag", "random_forest_error", "_bias_correction",
-           "_core_computation"]
+           "_core_computation","_gfit","_gbayes","_calibrateEB"]
 
 _due.cite(_BibTeX("""
 @ARTICLE{Wager2014-wn,
@@ -51,6 +54,118 @@ def calc_inbag(n_samples, forest):
         inbag[:, t_idx] = np.bincount(sample_idx[-1], minlength=n_samples)
     return inbag
 
+def _gbayes(x0, g_est, sigma):
+    """
+    Bayes posterior estimation with Gaussian noise.
+
+    Parameters
+    ----------
+    x0 : num
+        An observation.
+
+    g_est: DataFrame
+        A prior density (returned by _gfit).
+
+    sigma: num
+        Monte Carlo noise estimate.
+
+    Returns
+    -------
+    Posterior estimate E[mu|x0].
+    """
+    Kx = norm.pdf((g_est.x - x0) / sigma)
+    post = Kx * g_est.g
+    post /= np.sum(post)
+
+    return np.sum(post * g_est.x)
+
+def _gfit(X, sigma, p = 2, nbin = 1000, unif_fraction = 0.1):
+    """
+    Fit empirical Bayes prior in the hierarchical model
+    mu ~ G, X ~ N(mu,sigma^2)
+
+    Parameters
+    ----------
+    X : ndarray
+        A vector of observations.
+
+    sigma : num
+        Monte Carlo noise estimate.
+
+    p : int
+        Number of parameters used to fit G.
+
+    nbin : int
+        Number of bins used for discrete approximation.
+
+    unif_fraction : num
+        Fraction of G modeled as "slab"
+
+    Returns
+    -------
+    Posterior density estimate G (DataFrame)
+    """
+    xvals = np.linspace(np.min([np.min(X) - 2 * np.std(X), 0]), np.max([np.max(X) + 2 * np.std(X), np.std(X)]), nbin)
+    binw = xvals[1] - xvals[0]
+
+    zero_idx = np.max(np.where(xvals <= 0))
+    noise_kernel = norm.pdf(xvals / sigma) * binw / sigma
+
+    if zero_idx > 1:
+        noise_rotate = np.roll(noise_kernel,len(noise_kernel)-zero_idx)
+    else:
+        noise_rotate = noise_kernel
+
+    XX = np.array([xvals**i*(xvals>=0) for i in np.arange(1,p+1)]).T
+
+    def neg_loglik(eta):
+        g_eta_raw = np.exp(np.dot(XX,eta)) * (xvals >= 0)
+        if ((np.sum(g_eta_raw) == np.inf) | (np.sum(g_eta_raw) <= 100 * np.finfo(float).eps)):
+            return 1000 * (len(X) + np.sum(eta**2))
+
+        g_eta_main = g_eta_raw / np.sum(g_eta_raw)
+        g_eta = (1 - unif_fraction) * g_eta_main + unif_fraction * (xvals >= 0) / np.sum(xvals >= 0)
+        f_eta = np.convolve(g_eta, noise_rotate, mode = 'same')
+        return np.sum(np.interp(X, xvals, -np.log(np.maximum(f_eta, 0.0000001))))
+
+    eta_hat = minimize(neg_loglik, np.repeat(-1, p)).x
+    g_eta_raw = np.exp(np.dot(XX,eta_hat)) * (xvals >= 0)
+    g_eta_main = g_eta_raw / np.sum(g_eta_raw)
+    g_eta = (1 - unif_fraction) * g_eta_main + unif_fraction * (xvals >= 0) / np.sum(xvals >= 0)
+
+    return pd.DataFrame({'x': xvals, 'g': g_eta})
+
+def _calibrateEB(V_IJ_unbiased, sigma2):
+    """
+    Empirical Bayes calibration of noisy variance estimates.
+
+    Parameters
+    ----------
+    V_IJ_unbiased : ndarray
+        List of variance estimates.
+
+    sigma2 : num
+        Estimate of Monte Carlo noise in V_IJ_unbiased.
+
+    Returns
+    -------
+    Calibrated variance estimates.
+    """
+    if sigma2 <= 0 or np.min(V_IJ_unbiased) == np.max(V_IJ_unbiased):
+        return(np.maximum(V_IJ_unbiased, 0))
+
+    sigma = np.sqrt(sigma2)
+    eb_prior = _gfit(V_IJ_unbiased, sigma)
+
+    if len(V_IJ_unbiased >= 200):
+        # If there are many test points, use interpolation to speed up computations
+        calib_x = mstats.mquantiles(V_IJ_unbiased, prob = np.arange(0., 1.01, 0.02))
+        calib_y = np.array([_gbayes(xx,eb_prior,sigma) for xx in calib_x])
+        calib_all = np.interp(V_IJ_unbiased, calib_x, calib_y)
+    else:
+        calib_all = np.array([_gbayes(xx, eb_prior, sigma) for xx in V_IJ_unbiased])
+
+    return calib_all
 
 def _core_computation(X_train, X_test, inbag, pred_centered, n_trees):
     cov_hat = np.zeros((X_train.shape[0], X_test.shape[0]))
@@ -73,7 +188,7 @@ def _bias_correction(V_IJ, inbag, pred_centered, n_trees):
     return V_IJ_unbiased
 
 
-def random_forest_error(forest, inbag, X_train, X_test):
+def random_forest_error(forest, inbag, X_train, X_test, calibrate = True, used_trees = 'all'):
     """
     Calculates error bars from scikit-learn RandomForest estimators.
 
@@ -90,6 +205,12 @@ def random_forest_error(forest, inbag, X_train, X_test):
 
     X : ndarray
         An array with shape (n_sample, n_features).
+
+    calibrate : bool
+        Calibrate for MC effects, takes care of the negative values
+
+    used_trees : 'all' or ndarray
+        Use the full forest for the prediction or a subsample, with tree indices specified by the array.
 
     Returns
     -------
@@ -110,10 +231,37 @@ def random_forest_error(forest, inbag, X_train, X_test):
        Random Forests: The Jackknife and the Infinitesimal Jackknife", Journal
        of Machine Learning Research vol. 15, pp. 1625-1651, 2014.
     """
-    pred = np.array([tree.predict(X_test) for tree in forest]).T
-    pred_mean = np.mean(pred, 0)
-    pred_centered = pred - pred_mean
+
     n_trees = forest.n_estimators
-    V_IJ = _core_computation(X_train, X_test, inbag, pred_centered, n_trees)
-    V_IJ_unbiased = _bias_correction(V_IJ, inbag, pred_centered, n_trees)
+
+    if used_trees == 'all':
+        pred = np.array([tree.predict(X_test) for tree in forest]).T
+        pred_mean = np.mean(pred, 0)
+        pred_centered = pred - pred_mean
+        V_IJ = _core_computation(X_train, X_test, inbag, pred_centered, n_trees)
+        V_IJ_unbiased = _bias_correction(V_IJ, inbag, pred_centered, n_trees)
+    else:
+        pred = np.array([forest[i].predict(X_test) for i in used_trees]).T
+        pred_mean = np.mean(pred, 0)
+        pred_centered = pred - pred_mean
+        V_IJ = _core_computation(X_train, X_test, inbag[:,used_trees], pred_centered, len(used_trees))
+        V_IJ_unbiased = _bias_correction(V_IJ, inbag[:,used_trees], pred_centered, len(used_trees))
+
+    if calibrate:
+        # Compute variance estimates using half the trees
+        calibration_ratio = 2
+        n_samples = math.ceil(n_trees / calibration_ratio)
+
+        results_ss = random_forest_error(forest, inbag, X_train, X_test, calibrate = False,\
+                                        used_trees = np.random.choice(n_trees,n_samples))
+
+        # Use this second set of variance estimates to estimate scale of Monte Carlo noise
+        sigma2_ss = np.mean((results_ss - V_IJ_unbiased)**2)
+        delta = n_samples / n_trees
+        sigma2 = (delta**2 + (1 - delta)**2) / (2 * (1 - delta)**2) * sigma2_ss
+
+        # Use Monte Carlo noise scale estimate for empirical Bayes calibration
+        vars_calibrated = calibrateEB(V_IJ_unbiased, sigma2)
+        V_IJ_unbiased = vars_calibrated
+
     return V_IJ_unbiased
